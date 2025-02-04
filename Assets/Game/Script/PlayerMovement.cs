261a9560630521687befb5579f9a4efbcca2f6be
harelmarin <145020856+harelmarin@users.noreply.github.com>
2025-02-04 11:13:17 +0100

    [ADD] GrounCheck and jump

--- Assets/Game/Script/PlayerMovement.cs
+++ Assets/Game/Script/PlayerMovement.cs
@@ -3,9 +3,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Mouvement")]
     [SerializeField] private float vitesseDeplacement = 5f;
+    [SerializeField] private float forceJump = 12f;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Vector2 groundCheckSize = new Vector2(0.1f, 0.05f);
+    [SerializeField] private LayerMask groundLayer;
+
     private Rigidbody2D rb;
     private float mouvementHorizontal;
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -17,8 +26,23 @@ public class PlayerMovement : MonoBehaviour
         mouvementHorizontal = context.ReadValue<Vector2>().x;
     }
 
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.AddForce(Vector2.up * forceJump, ForceMode2D.Impulse);
+        }
+    }
+
     private void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapBox(
+            groundCheck.position,
+            groundCheckSize,
+            0f,
+            groundLayer
+        );
+        
         rb.linearVelocity = new Vector2(mouvementHorizontal * vitesseDeplacement, rb.linearVelocity.y);
     }
 }
